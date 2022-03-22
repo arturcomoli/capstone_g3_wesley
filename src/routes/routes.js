66487d7e0e7d8ab01dddaad2effb584783f73104
Artur Comoli <artur.comoli@gmail.com>
2022-03-22 10:18:08 -0300

    fix: routes pathing fixed

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,30 @@ import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
+// import LandingPage from "../pages/LandingPage";
+// import Dashboard from "../paged/Dashboard";
+// import Profile from "../pages/Profile";
+// import Routine from "../pages/Routine";
+// import Team from "../pages/Team";
+// import Exercises from "../pages/Exercises"
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
+        {/* <LandingPage/> */}
+      </Route>
+      <Route path="/login">
         <Login />
       </Route>
       <Route path="/signup">
         <Signup />
       </Route>
+      <Route path="/dashboard">{/* <Dashboard/> */}</Route>
+      <Route path="/profile">{/* <Profile/> */}</Route>
+      <Route path="/routine">{/* <Routine/> */}</Route>
+      <Route path="/exercises">{/* <Exercises/> */}</Route>
+      <Route path="/team">{/* <Team/> */}</Route>
     </Switch>
   );
 };
